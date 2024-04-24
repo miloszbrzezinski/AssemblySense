1321bcf53acc2ebaa5d5d77e3db246b61b83b984
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-04-24 15:02:19 +0200

    Addres input in networks table changed: <input> vs states, control by keys added

--- components/projects/design/network/network-table/address-input.tsx
+++ components/projects/design/network/network-table/address-input.tsx
@@ -13,7 +13,18 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { ProjectNetworkWithData } from "@/types";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
+
+interface MaskStates {
+  maskFirst: number[];
+  maskSecond: number[];
+  maskThird: number[];
+  maskFourth: number[];
+  ipFirst: number[];
+  ipSecond: number[];
+  ipThird: number[];
+  ipFourth: number[];
+}
 
 interface NetworkAddressInputProps {
   profileId: string;
@@ -26,17 +37,96 @@ export const NetworkAddressInput = ({
   workspaceId,
   projectNetwork,
 }: NetworkAddressInputProps) => {
-  const [maskFirst, setMaskFirst] = useState("");
-  const [maskSecond, setMaskSecond] = useState("");
-  const [maskThird, setMaskThird] = useState("");
-  const [maskFourth, setMaskFourth] = useState("");
+  const [maskIndex, setMaskIndex] = useState(0);
+  const [address, setAddress] = useState<MaskStates>({
+    maskFirst: [],
+    maskSecond: [],
+    maskThird: [],
+    maskFourth: [],
+    ipFirst: [],
+    ipSecond: [],
+    ipThird: [],
+    ipFourth: [],
+  });
 
-  const handleInputMastFirst = (event: FormEvent<HTMLInputElement>) => {
-    setMaskFirst(event.currentTarget.value);
-    if (parseInt(maskFirst) > 255) {
-      setMaskFirst("255");
-    }
-  };
+  useEffect(() => {
+    const keyDownHandler = (e: KeyboardEvent) => {
+      let element: keyof typeof address = "maskFirst";
+      switch (maskIndex) {
+        case 0:
+          element = "maskFirst";
+          break;
+        case 1:
+          element = "maskSecond";
+          break;
+        case 2:
+          element = "maskThird";
+          break;
+        case 3:
+          element = "maskFourth";
+          break;
+        case 4:
+          element = "ipFirst";
+          break;
+        case 5:
+          element = "ipSecond";
+          break;
+        case 6:
+          element = "ipThird";
+          break;
+        case 7:
+          element = "ipFourth";
+          break;
+      }
+      if (Number(e.key) || Number(e.key) === 0) {
+        const tmpMask = [...address[element], Number(e.key)];
+        console.log(tmpMask.length);
+        if (tmpMask.length < 4) {
+          setAddress((prevAddress) => ({
+            ...prevAddress,
+            [element]: tmpMask,
+          }));
+        }
+        if (tmpMask.length >= 3) {
+          setMaskIndex((prevIndex) => prevIndex + 1);
+        }
+      }
+      if (e.key === "Backspace") {
+        const tmpMask = [...address[element]];
+        const tmpMaskIndex = maskIndex;
+        if (tmpMask.length === 0 && tmpMaskIndex > 0) {
+          setMaskIndex((prevIndex) => prevIndex - 1);
+        }
+        if (tmpMask.length > 0) {
+          tmpMask.pop();
+          setAddress((prevAddress) => ({
+            ...prevAddress,
+            [element]: tmpMask,
+          }));
+        }
+      }
+      const tmpMaskIndex = maskIndex;
+      if (
+        (e.key === "Tab" ||
+          e.key === "ArrowRight" ||
+          e.key === "ArrowDown" ||
+          e.key === "Enter") &&
+        tmpMaskIndex < 7
+      ) {
+        setMaskIndex((prevIndex) => prevIndex + 1);
+      }
+      if ((e.key === "ArrowLeft" || e.key === "ArrowUp") && tmpMaskIndex > 0) {
+        setMaskIndex((prevIndex) => prevIndex - 1);
+      }
+    };
+    document.addEventListener("keydown", keyDownHandler);
+
+    // clean up
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [maskIndex, address]);
+
   return (
     <Popover>
       <PopoverTrigger className="text-base h-10 w-full flex items-center justify-start hover:bg-slate-200">
@@ -47,40 +137,93 @@ export const NetworkAddressInput = ({
       <PopoverContent className="rounded-none p-0 bg-white space-y-[1px] w-full">
         <div className="p-2">
           <h3 className="font-light pb-2">Subnet mask</h3>
-          <div className="border">
-            <input
-              type="number"
-              value={maskFirst}
-              onChange={handleInputMastFirst}
+          <div className="flex">
+            <div
+              onClick={() => setMaskIndex(0)}
               className={cn(
-                "w-10 text-center text-lg",
-                parseInt(maskFirst) > 255 && "bg-red-200",
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 0 && "bg-slate-200",
               )}
-            />
+            >
+              {address.maskFirst}
+            </div>
+
             <span>.</span>
-            <input className="w-10 text-center text-lg" />
+            <div
+              onClick={() => setMaskIndex(1)}
+              className={cn(
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 1 && "bg-slate-200",
+              )}
+            >
+              {address.maskSecond}
+            </div>
             <span>.</span>
-            <input className="w-10 text-center text-lg" />
+            <div
+              onClick={() => setMaskIndex(2)}
+              className={cn(
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 2 && "bg-slate-200",
+              )}
+            >
+              {address.maskThird}
+            </div>
             <span>.</span>
-            <input className="w-10 text-center text-lg" />
+            <div
+              onClick={() => setMaskIndex(3)}
+              className={cn(
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 3 && "bg-slate-200",
+              )}
+            >
+              {address.maskFourth}
+            </div>
           </div>
         </div>
         <Separator />
         <div className="p-2">
           <h3 className="font-light pb-2">Network portion</h3>
-          <div className="border">
-            <input className="w-10 text-center text-lg" />
-            <span>.</span>
-            <input className="w-10 text-center text-lg" />
-            <span>.</span>
-            <input className="w-10 text-center text-lg" />
-            <span className={cn(parseInt(maskFirst) !== 0 && "hidden")}>.</span>
-            <input
+          <div className="flex">
+            <div
+              onClick={() => setMaskIndex(4)}
               className={cn(
-                "w-10 text-centertext-lg hide",
-                parseInt(maskFirst) !== 0 && "hidden",
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 4 && "bg-slate-200",
               )}
-            />
+            >
+              {address.ipFirst}
+            </div>
+
+            <span>.</span>
+            <div
+              onClick={() => setMaskIndex(5)}
+              className={cn(
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 5 && "bg-slate-200",
+              )}
+            >
+              {address.ipSecond}
+            </div>
+            <span>.</span>
+            <div
+              onClick={() => setMaskIndex(6)}
+              className={cn(
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 6 && "bg-slate-200",
+              )}
+            >
+              {address.ipThird}
+            </div>
+            <span>.</span>
+            <div
+              onClick={() => setMaskIndex(7)}
+              className={cn(
+                "flex w-10 h-6 hover:bg-slate-100 text-lg items-center justify-center",
+                maskIndex === 7 && "bg-slate-200",
+              )}
+            >
+              {address.ipFourth}
+            </div>
           </div>
         </div>
         <Separator />
