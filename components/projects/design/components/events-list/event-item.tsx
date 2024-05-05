395a34e41bc04204d6d10c10473e7d2f3cd3fb39
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-05-06 01:36:05 +0200

    event list in component page: added dropdown menu with problems reporting

--- components/projects/design/components/events-list/event-item.tsx
+++ components/projects/design/components/events-list/event-item.tsx
@@ -4,11 +4,18 @@ import { ComponentEventName } from "./event-name";
 import { ComponentEventDescription } from "./event-description";
 import { ComponentEventSymbol } from "./event-symbol";
 import { ComponentEventAddress } from "./event-address";
-import { X } from "lucide-react";
+import { Flag, MoreVertical, Trash, X } from "lucide-react";
 import { startTransition } from "react";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { removeProjectComponentEvent } from "@/actions/component-event";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface EventListItemProps {
   profileId: string;
@@ -24,6 +31,7 @@ export const EventListItem = ({
   projectComponent,
 }: EventListItemProps) => {
   const router = useRouter();
+  const { onOpen } = useModal();
 
   const removeEvent = () => {
     startTransition(() => {
@@ -50,19 +58,54 @@ export const EventListItem = ({
     });
   };
 
-
   return (
     <tr className="group h-10">
       <td className="group-hover:bg-slate-100 border border-l-0 border-stone-300">
-        <button
-          onClick={removeEvent}
-          className="hover:bg-red-200 flex items-center justify-center h-10 w-full"
-        >
-          <X
-            strokeWidth={1}
-            className="hidden group-hover:block"
-          />
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="hover:bg-slate-200 flex items-center justify-center h-10 w-full outline-none">
+            <MoreVertical
+              strokeWidth={1}
+              className="hidden group-hover:block"
+            />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="left">
+            <DropdownMenuItem
+              onClick={() => {
+                onOpen("reportProjectProblem", {
+                  profileId,
+                  workspaceId,
+                  projectId: projectComponent.projectId,
+                  addressIO: event.addressIO!,
+                });
+              }}
+              className="text-stone-900"
+            >
+              <Flag className="h-4 w-4 mr-2" />
+              Report address problem
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => {
+                onOpen("reportProjectProblem", {
+                  profileId,
+                  workspaceId,
+                  projectId: projectComponent.projectId,
+                  componentEvent: event,
+                });
+              }}
+              className="text-stone-900"
+            >
+              <Flag className="h-4 w-4 mr-2" />
+              Report event problem
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className="text-red-900 hover:bg-red-200"
+              onClick={removeEvent}
+            >
+              <Trash className="h-4 w-4 mr-2" />
+              Remove
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </td>
       <td className="group-hover:bg-slate-100 border bg-white border-t-0 border-l-0 border-stone-300 min-w-36 text-sm font-light whitespace-nowrap">
         <ComponentEventName
