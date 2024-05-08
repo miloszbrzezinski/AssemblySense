d457fddaaa8ff5e08ccbcd136d24510ef04aaae5
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-05-08 02:45:44 +0200

    drag and drop to the project stages list added

--- components/projects/settings/project-stages-list.tsx
+++ components/projects/settings/project-stages-list.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 import { setActiveProjectStage } from "@/actions/project-stage";
 import { Button } from "@/components/ui/button";
 import { useModal } from "@/hooks/use-modal-store";
@@ -6,7 +7,7 @@ import { cn } from "@/lib/utils";
 import { ProjectStage } from "@prisma/client";
 import { Edit, Grip, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { startTransition } from "react";
+import { startTransition, useState } from "react";
 import { toast } from "sonner";
 
 interface ProjectStagesListProps {
@@ -24,6 +25,7 @@ export const ProjectStagesList = ({
 }: ProjectStagesListProps) => {
   const router = useRouter();
   const { onOpen } = useModal();
+  const [stagesList, setStagesList] = useState(projectStages);
 
   const onActivate = (stageId: string) => {
     startTransition(() => {
@@ -41,81 +43,131 @@ export const ProjectStagesList = ({
     });
   };
 
+  const reorder = (list: any[], startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+    //console.log(`source: ${source.index} destination: ${destination.index}`);
+    const newList = [...stagesList];
+    console.log(
+      `${newList[source.index].name} moved to pos of ${
+        newList[destination.index].name
+      }`
+    );
+    const items = reorder(
+      newList,
+      result.source.index,
+      result.destination.index
+    );
+    setStagesList(items);
+  };
+
   return (
-    <div className="flex w-full h-full p-5 shadow-md">
-      <ol className="w-full shadow-md h-min space-y-[1px] bg-stone-300">
-        {projectStages.map((stage) => (
-          <li
-            key={stage.id}
-            className="w-full bg-white items-center flex p-5 pl-2 justify-between"
-          >
-            <div className="flex space-x-2 items-center">
-              <Grip className="text-stone-300" />
-              <div>
-                <p className={cn(stage.active && "font-medium underline")}>
-                  {stage.order + 1}. {stage.name}
-                </p>
-                <p className="space-x-2">
-                  {stage.startDate && (
-                    <span className="font-light">
-                      {new Date(stage.startDate).getDate()}/
-                      {new Date(stage.startDate).getMonth()}/
-                      {new Date(stage.startDate).getFullYear()}
-                    </span>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="flex w-full h-full p-5 shadow-md">
+        <Droppable droppableId={"projectStage"} type="projectStage">
+          {(provided) => (
+            <ol
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="w-full shadow-md h-min space-y-[1px] bg-stone-300"
+            >
+              {stagesList.map((stage, index) => (
+                <Draggable key={stage.id} draggableId={stage.id} index={index}>
+                  {(provided) => (
+                    <li
+                      {...provided.draggableProps}
+                      ref={provided.innerRef}
+                      className="w-full bg-white items-center flex p-5 pl-2 justify-between"
+                    >
+                      <div className="flex space-x-2 items-center">
+                        <div
+                          {...provided.dragHandleProps}
+                          className="flex h-10 items-center"
+                        >
+                          <Grip className="text-stone-300" />
+                        </div>
+                        <div>
+                          <p
+                            className={cn(
+                              stage.active && "font-medium underline"
+                            )}
+                          >
+                            {stage.order + 1}. {stage.name}
+                          </p>
+                          <p className="space-x-2">
+                            {stage.startDate && (
+                              <span className="font-light">
+                                {new Date(stage.startDate).getDate()}/
+                                {new Date(stage.startDate).getMonth()}/
+                                {new Date(stage.startDate).getFullYear()}
+                              </span>
+                            )}
+                            {stage.deadline && (
+                              <>
+                                <span>-</span>
+                                <span className="font-light">
+                                  {new Date(stage.deadline).getDate()}/
+                                  {new Date(stage.deadline).getMonth()}/
+                                  {new Date(stage.deadline).getFullYear()}
+                                </span>
+                              </>
+                            )}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="flex space-x-2 items-center">
+                        <Button
+                          onClick={() => {
+                            onActivate(stage.id);
+                          }}
+                          disabled={stage.active}
+                          variant={stage.active ? "secondary" : "outline"}
+                        >
+                          {stage.active ? "Activated" : "Active"}
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            onOpen("editProjectStage", {
+                              profileId,
+                              workspaceId,
+                              projectId: stage.projectId,
+                              projectStage: stage,
+                            });
+                          }}
+                          variant="outline"
+                        >
+                          <Edit strokeWidth={1} />
+                        </Button>
+                        <Button
+                          onClick={() => {
+                            onOpen("removeProjectStage", {
+                              profileId,
+                              workspaceId,
+                              projectId: stage.projectId,
+                              projectStageId: stage.id,
+                            });
+                          }}
+                          variant="outline"
+                        >
+                          <Trash strokeWidth={1} />
+                        </Button>
+                      </div>
+                    </li>
                   )}
-                  {stage.deadline && (
-                    <>
-                      <span>-</span>
-                      <span className="font-light">
-                        {new Date(stage.deadline).getDate()}/
-                        {new Date(stage.deadline).getMonth()}/
-                        {new Date(stage.deadline).getFullYear()}
-                      </span>
-                    </>
-                  )}
-                </p>
-              </div>
-            </div>
-            <div className="flex space-x-2 items-center">
-              <Button
-                onClick={() => {
-                  onActivate(stage.id);
-                }}
-                disabled={stage.active}
-                variant={stage.active ? "secondary" : "outline"}
-              >
-                {stage.active ? "Activated" : "Active"}
-              </Button>
-              <Button
-                onClick={() => {
-                  onOpen("editProjectStage", {
-                    profileId,
-                    workspaceId,
-                    projectId: stage.projectId,
-                    projectStage: stage,
-                  });
-                }}
-                variant="outline"
-              >
-                <Edit strokeWidth={1} />
-              </Button>
-              <Button
-                onClick={() => {
-                  onOpen("removeProjectStage", {
-                    profileId,
-                    workspaceId,
-                    projectId: stage.projectId,
-                    projectStageId: stage.id,
-                  });
-                }}
-                variant="outline"
-              >
-                <Trash strokeWidth={1} />
-              </Button>
-            </div>
-          </li>
-        ))}
-      </ol>
-    </div>
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </ol>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 };
