c631c3bab4a65e3cf732b13bb56390aeee4388d9
miloszbrzezinski <109414155+miloszbrzezinski@users.noreply.github.com>
2024-05-13 19:11:09 +0200

    working hours table added

--- app/workspace/[workspaceId]/working-time/page.tsx
+++ app/workspace/[workspaceId]/working-time/page.tsx
@@ -1,21 +1,35 @@
-import {
-  format,
-  startOfMonth,
-  endOfMonth,
-  startOfWeek,
-  endOfWeek,
-  addDays,
-  eachDayOfInterval,
-  addMonths,
-} from "date-fns";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { MoreVertical, Timer } from "lucide-react";
 
-export default function WorkTimePage({
+export default async function WorkTimePage({
   params,
   searchParams,
 }: {
-  params: { day: string };
+  params: { workspaceId: string };
   searchParams?: { [key: string]: string | string[] | undefined };
 }) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return;
+  }
+
+  const workspace = await db.workspace.findUnique({
+    where: {
+      id: params.workspaceId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!workspace) {
+    return;
+  }
+
   const date = new Date(
     `${searchParams?.year}-${searchParams?.month}-${searchParams?.day}`
   );
@@ -43,12 +57,92 @@ export default function WorkTimePage({
     "December",
   ];
 
+  const workingHours = await db.workingHours.findMany({
+    where: {
+      projectMember: {
+        workspaceMember: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      projectMember: {
+        include: {
+          project: true,
+        },
+      },
+      assemblyGroup: true,
+      process: {
+        include: {
+          assemblyGroup: true,
+        },
+      },
+    },
+  });
+
   return (
     <div className="w-full flex flex-col">
       <div className="flex w-full items-center p-5">
         <p className="text-3xl font-light">
           {weekDays[date.getDay()]}, {date.getDate()} {months[date.getMonth()]}
         </p>
+      </div>
+      <div className="p-5">
+        <table className="border-collapse shadow-md relative w-full bg-white">
+          <thead className="h-14">
+            <tr>
+              <th className="sticky top-0 border bg-neutral-100/90 border-l-1 border-t-1 border-stone-300 w-10 min-w-10">
+                <div className="flex items-center justify-center">
+                  <Timer strokeWidth={1} />
+                </div>
+              </th>
+              <th className="sticky top-0 border bg-neutral-100/95 border-t-1 border-stone-300 min-w-36 text-base font-light whitespace-nowrap px-6">
+                Time
+              </th>
+              <th className="sticky top-0 border bg-neutral-100/95 border-t-1 border-stone-300 min-w-36 text-base font-light whitespace-nowrap px-6">
+                Project
+              </th>
+              <th className="sticky top-0 border bg-neutral-100/95 border-t-1 border-stone-300 min-w-36 text-base font-light whitespace-nowrap px-6">
+                Group
+              </th>
+              <th className="sticky top-0 border bg-neutral-100/95 border-t-1 border-stone-300 min-w-36 text-base font-light whitespace-nowrap px-6">
+                Process
+              </th>
+              <th className="sticky top-0 border bg-neutral-100/95 border-t-1 border-stone-300 min-w-36 text-base font-light whitespace-nowrap px-6">
+                Source
+              </th>
+            </tr>
+          </thead>
+          <tbody className="pb-20">
+            {workingHours.map((wh) => (
+              <tr className="group h-10">
+                <td className="group-hover:bg-slate-100 border border-l-1 border-stone-300">
+                  <button className="hover:bg-slate-200 flex items-center justify-center h-10 w-full">
+                    <MoreVertical
+                      strokeWidth={1}
+                      className="hidden group-hover:block"
+                    />
+                  </button>
+                </td>
+                <td className="group-hover:bg-slate-100 border border-stone-300">
+                  {wh.value}h
+                </td>
+                <td className="group-hover:bg-slate-100 border border-stone-300">
+                  {wh.projectMember.project.id}: {wh.projectMember.project.name}
+                </td>
+                <td className="group-hover:bg-slate-100 border border-stone-300">
+                  {wh.assemblyGroup?.name}
+                </td>
+                <td className="group-hover:bg-slate-100 border border-stone-300">
+                  {wh.process?.processId} {wh.process?.name}
+                </td>
+                <td className="group-hover:bg-slate-100 border border-stone-300">
+                  S O U R C E
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
